fc60f1292a2578c87f88a6bb9d986701b43ab9fe
jeijoel <jeijoel@estudiantec.cr>
2025-05-28 18:32:12 -0600

    Agregar archivos de proyecto.

---
+++ Quiz4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public Array[] UnirIntervalosTraslapado(Array[] arreglo)
+        {    
+            // Implementación del método para unir intervalos traslapados
+            int i = 0;
+            while i <
+            return new Array[] { }; // Retorna un array vacío por ahora} 
+        }
+}
