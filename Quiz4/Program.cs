9ac2e438bd54362fbf843507d36c9d40c2613ce4
Ivan Brito <ibrito@estudiantec.cr>
2025-05-28 20:08:46 -0600

    pregunta 2

--- Quiz4/Program.cs
+++ Quiz4/Program.cs
@@ -13,4 +13,30 @@
             while i <
             return new Array[] { }; // Retorna un array vacío por ahora} 
         }
+        public string DeterminarAnagrama(string string1, string, string2) 
+        {
+            // Dividir el string en un array de characteres
+            char[] Array1 = string1.ToCharArray();
+            char[] Array2 = string2.ToCharArray(); 
+            // Verificar si la longituid de las palabras es la misma
+            if (Array1.length == Array2.length) 
+            {
+                for (i = 0; i < Array1.length; i++); 
+                {
+                    // 2do for loop
+                    for (j = 0; j < Array2.length; j++);
+                    if (Array1[i] != Array2[j]) 
+                    {
+                        Console.WriteLine("Los strings no son anagramas.");
+                    } else {
+                        Console.WriteLine("Los strings son anagramas.");
+                    }
+                }
+            } else 
+            {
+                Console.WriteLine("Las strings pasados no son anagramas porque no tienen la misma cantidad de caracteres.");
+                return -1;
+            }
+
+        }
 }
